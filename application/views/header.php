b3506db09f475eac2ef3f34883692e16a113f51d
diaz26 <jeffreydiazaya@outlook.com>
2019-04-01 22:15:32 -0500

    .

--- application/views/header.php
+++ application/views/header.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="<?= base_url(); ?>jquery.mobile/jquery.mobile-1.4.5.min.css">
   <link rel="stylesheet" href="<?= base_url(); ?>jquery.mobile/jquery.mobile.icons-1.4.5.min.css">
   <link rel="stylesheet" href="<?= base_url(); ?>jquery.mobile/jquery.mobile.structure-1.4.5.min.css">
-
+  
   <link rel="stylesheet" href="<?= base_url(); ?>themes/jeffthems.min.css">
   <link rel="stylesheet" href="<?= base_url(); ?>themes/jquery.mobile.icons.min.css">
 </head>
--- application/views/view_home.php
+++ application/views/view_home.php
@@ -18,7 +18,21 @@
       </nav>
     </div>
     <div data-role="main">
-        
+      <ul data-role="listview">
+        <li style="padding-top: 0px;padding-bottom: 0px"><p style="font-size:14px">Compra tus repuestos desde la comodidad de tu hogar</p></li>
+        <li style="padding-top: 0px;padding-bottom: 0px"><p style="font-size:14px">Envíos Nacionales <img src="<?= base_url(); ?>jquery.mobile/demos/_assets/img/col.png" width="5%" class="ui-li-icon"> Garantizados</p></li>
+        <li style="padding-top: 0px;padding-bottom: 0px"><p style="font-size:14px">Respuestos importados 100% originales</p></li>
+      </ul>
+      <h3 style="font-size:hughs; text-align:center; color: red;">¡MotoRepuestos JeffPi, tu vendedor confiable!</h3>
+      <ul data-role="listview">
+        <li data-role="list-divider" style="text-align:center">Algunos de nuestros productos</li>
+      </ul>
+      <div class="ui-content" data-role="popup" id="compra" data-theme="a" style="max-width:340px; padding-bottom:2em">
+        <h3>Artículo de compra</h3>
+        <p>Este artícuo contiene las cosas necesarias para que lo compre</p>
+        <a href="#" data-rel="back" class="ui-shadow ui-btn ui-corner-all ui-btn-b ui-icon-check ui-icon-left ui-mini">Compra: 50.00 Buscando</a>
+        <a href="#" data-rel="back" class="ui-shadow ui-btn ui-corner-all ui-mini">Cancelar</a>
+      </div>
     </div>
     <div data-role="footer">
 
