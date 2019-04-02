b4a9a20e85ad9f2215d0685e430dc1029c554820
diaz26 <jeffreydiazaya@outlook.com>
2019-04-01 22:40:27 -0500

    .

--- application/views/view_home.php
+++ application/views/view_home.php
@@ -23,15 +23,21 @@
         <li style="padding-top: 0px;padding-bottom: 0px"><p style="font-size:14px">Envíos Nacionales <img src="<?= base_url(); ?>jquery.mobile/demos/_assets/img/col.png" width="5%" class="ui-li-icon"> Garantizados</p></li>
         <li style="padding-top: 0px;padding-bottom: 0px"><p style="font-size:14px">Respuestos importados 100% originales</p></li>
       </ul>
-      <h3 style="font-size:hughs; text-align:center; color: red;">¡MotoRepuestos JeffPi, tu vendedor confiable!</h3>
       <ul data-role="listview">
         <li data-role="list-divider" style="text-align:center">Algunos de nuestros productos</li>
       </ul>
-      <div class="ui-content" data-role="popup" id="compra" data-theme="a" style="max-width:340px; padding-bottom:2em">
-        <h3>Artículo de compra</h3>
-        <p>Este artícuo contiene las cosas necesarias para que lo compre</p>
-        <a href="#" data-rel="back" class="ui-shadow ui-btn ui-corner-all ui-btn-b ui-icon-check ui-icon-left ui-mini">Compra: 50.00 Buscando</a>
-        <a href="#" data-rel="back" class="ui-shadow ui-btn ui-corner-all ui-mini">Cancelar</a>
+      <div class="ui-grid-a">
+        
+        <div class="ui-block-c">
+          <div class="ui-bar ui-bar-a" style="background-color:#B2F7D9;text-align: center">Cúpula frontal  - MT 09</div>
+          <div class="ui-bar ui-bar-a" style="background-color:#E1E1E1"><img src="<?= base_url(); ?>jquery.mobile/images/cupula.jpg" width="100%"> </div>
+          <div class="ui-bar ui-bar-a" style="background-color:#B2F7D9;text-align: center">Carenaje - R1</div>
+          <div class="ui-bar ui-bar-a" style="background-color:#E1E1E1"><img src="<?= base_url(); ?>jquery.mobile/images/yamaha.jpg" width="100%"> </div>
+            <div class="ui-bar ui-bar-a" style="background-color:#B2F7D9;text-align: center">Kit de arrastre - Evo R3</div>
+            <div class="ui-bar ui-bar-a" style="background-color:#E1E1E1"><img src="<?= base_url(); ?>jquery.mobile/images/kitarr.jpg" width="100%"> </div>
+            <div class="ui-bar ui-bar-a" style="background-color:#B2F7D9;text-align: center">Sliders - MT 07</div>
+            <div class="ui-bar ui-bar-a" style="background-color:#E1E1E1"><img src="<?= base_url(); ?>jquery.mobile/images/sliders.jpg" width="100%"> </div>
+        </div>
       </div>
     </div>
     <div data-role="footer">
